a4e742f4248a26c113a952f70a441df64a99bcca
SuprHUlk <ayush123kumar.ak@gmail.com>
2024-03-31 19:32:23 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-const parseQuery = require("./queryParser");
-const readCSV = require("./csvReader");
+const { readCSV, writeCSV } = require("./readCSV");
+const {
+  parseSelectQuery,
+  parseInsertQuery,
+  parseDeleteQuery,
+} = require("./queryParser");
+const {
+  executeSELECTQuery,
+  executeINSERTQuery,
+  executeDELETEQuery,
+} = require("./executeSELECTQuery");
 
-async function executeSELECTQuery(query) {
-  const { fields, table, whereClause } = parseQuery(query);
-  const data = await readCSV(`${table}.csv`);
-
-  // Filtering based on WHERE clause
-  const filteredData = whereClause
-    ? data.filter((row) => {
-        const [field, value] = whereClause.split("=").map((s) => s.trim());
-        return row[field] === value;
-      })
-    : data;
-
-  // Selecting the specified fields
-  return filteredData.map((row) => {
-    const selectedRow = {};
-    fields.forEach((field) => {
-      selectedRow[field] = row[field];
-    });
-    return selectedRow;
-  });
-}
-
-module.exports = executeSELECTQuery;
+module.exports = {
+  readCSV,
+  writeCSV,
+  executeSELECTQuery,
+  executeINSERTQuery,
+  executeDELETEQuery,
+  parseSelectQuery,
+  parseInsertQuery,
+  parseDeleteQuery,
+};
